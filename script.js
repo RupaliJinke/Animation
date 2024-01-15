1fcc07c7aa5f2dfb901d94299898eaeedc3c9ecf
RupaliJinke <143118879+RupaliJinke@users.noreply.github.com>
2024-01-15 17:59:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var t1=gsap.timeline();
+
+t1.to("#loader",{
+    top:"-100vh",
+    delay:4,
+    duartion:2
+
+})
+
+t1.from("#navbar",{
+    y: -200,
+    duration:2,
+    delay:1,
+}
+)
+t1.from("#part1, #part2, #part3",{
+    y: -100,
+    duration:1,
+    opacity:00,
+    stagger:0.5,
+
+})
+t1.from("#main h1", {
+    x: -1500,
+    duration: 2,
+   stagger:0.5
+})
+
+t1.from("#main img", {
+    x:100,
+    opacity:0
+})
